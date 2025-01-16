10507145c980373365894d14e995c40219a0303d
SejdiajMarlis <marlissejdiaj@gmail.com>
2025-01-16 11:56:16 +0000

    Define abstract class Person with name and age attributes.

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import csv
 import pandas as pd
 import time
 from abc import ABC, abstractmethod
+
+class Person(ABC):
+    @abstractmethod
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
